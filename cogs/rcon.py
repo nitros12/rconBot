cc53e7de8fcc659ff64121b706512517d950a5d5
nitros12 <bsimms.simms@gmail.com>
2017-06-08 21:31:01 +0100

    yeah

--- cogs/rcon.py
+++ cogs/rcon.py
@@ -49,7 +49,9 @@ class Rcon:
     @commands.command()
     async def add_role(self, ctx, role: discord.Role):
         """Allow a role to list and command RCON connections.
-        Note only one role is assigned per guild. Setting this when a role already is set will override."""
+
+        Note only one role is assigned per guild. Setting this when a role already is set will override.
+        """
         await self.bot.redis.set(f"{ctx.guild.id}:rcon_role", str(role.id))
         await ctx.send(f"Set {role} as controlling rcon role for this guild!")
 
@@ -57,7 +59,7 @@ class Rcon:
     @commands.command()
     async def delete_role(self, ctx):
         """Delete role for accessing rcon."""
-        await self.bot.redis.delete(f"{ctx.guild.id}:rcon_role")
+        await self.bot.redis.delete([f"{ctx.guild.id}:rcon_role"])
         await ctx.send("Removed role controlling rcon role for this guild!")
 
     @check_redis_roles()
@@ -79,7 +81,7 @@ class Rcon:
     @check_redis_roles()
     @commands.command(name="list")
     async def list_cmd(self, ctx):
-        keys = await self.bot.redis.keys(f"{ctx.guild.id}:rcon_connections:")
+        keys = await self.bot.redis.keys(f"{ctx.guild.id}:rcon_connections:*")
         filtered = "\n".join(i.split(':')[-1] for i in keys)
         await ctx.send(f"```\n{filtered}```")
 
