e5799fc7fae54be482efe6eca848695375fcd14f
nitros12 <ben@bensimms.moe>
2017-09-19 12:10:07 +0100

    Recursively fold dict

--- conf.py
+++ conf.py
@@ -6,12 +6,20 @@ def ddict(*args, **kwargs):
     return defaultdict(ddict, *args, **kwargs)
 
 
-def build_dict(d):
+def build_ddict(d):
     def b(value):
         if isinstance(value, dict):
-            return build_dict(value)
+            return build_ddict(value)
         return value
     return ddict((k, b(v)) for k, v in d.items())
+
+
+def build_dict(d):
+    def b(value):
+        if isinstance(value, defaultdict):
+            return build_dict(value)
+        return value
+    return dict((k, b(v)) for k, v in d.items())
 
 
 class Config:
@@ -29,7 +37,7 @@ class Config:
 
     def _load(self):
         with open(self.fname) as fp:
-            self.data = build_dict(self.decoder.load(fp))
+            self.data = build_ddict(self.decoder.load(fp))
 
     async def load(self):
         async with self.lock:
@@ -37,7 +45,7 @@ class Config:
 
     def _save(self):
         with open(self.fname, "w") as fp:
-            self.decoder.dump(dict(self.data), fp)
+            self.decoder.dump(build_dict(self.data), fp)
 
     async def save(self):
         async with self.lock:
